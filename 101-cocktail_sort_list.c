d741c756f0c114e9729df76773b6f1f8cec4b2d4
josephorokpo <josephorokpo@gmail.com>
2023-11-17 05:00:08 -0800

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,51 @@
 #include "sort.h"
 
 /**
+ * print_list - Prints a doubly linked list of integers
+ * @list: Pointer to the head of the doubly linked list
+ */
+void print_list(const listint_t *list)
+{
+	while (list)
+	{
+		printf("%d", list->n);
+		list = list->next;
+		if (list)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @node: Pointer to the node to be swapped with its next node
+ * @list: Double pointer to the head of the doubly linked list
+ */
+void swap_nodes(listint_t *node, listint_t **list)
+{
+	listint_t *temp;
+
+	if (node == NULL || node->next == NULL)
+		return;
+
+	temp = node->prev;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+
+	if (node->next != NULL)
+		node->next->prev = node;
+
+	if (temp != NULL)
+		temp->next = node;
+	else
+		*list = node;
+
+	if (node->next != NULL)
+		node->next->prev = temp;
+}
+
+/**
  * cocktail_sort_list - Sorts a doubly linked list of integers in ascending
  *                      order using the Cocktail Shaker sort algorithm.
  * @list: Double pointer to the head of the doubly linked list
@@ -9,12 +54,12 @@ void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
 	listint_t *current;
-	listint_t *temp;
 
 	if (list == NULL || *list == NULL)
 		return;
 
-	do {
+	do
+	{
 		swapped = 0;
 		current = *list;
 
@@ -48,29 +93,4 @@ void cocktail_sort_list(listint_t **list)
 				current = current->prev;
 		}
 	} while (swapped);
-}
-
-/**
- * swap_nodes - Swaps two nodes in a doubly linked list.
- * @node: Pointer to the node to be swapped with its next node
- * @list: Double pointer to the head of the doubly linked list
- */
-void swap_nodes(listint_t *node, listint_t **list)
-{
-	if (node == NULL || node->next == NULL)
-		return;
-
-	if (node->prev != NULL)
-		node->prev->next = node->next;
-	else
-		*list = node->next;
-
-	node->next->prev = node->prev;
-	node->prev = node->next;
-	node->next = node->next->next;
-
-	if (node->next != NULL)
-		node->next->prev = node;
-
-	node->prev->next = node;
 }
